127a03ab0b367c145b11c730779cecc7f99a210b
Mike Merzliakov <2281205@gmail.com>
2023-05-01 01:04:15 +0300

    add HW01-base

---
+++ HW01_WelcomeToNode/index.js
@@ -0,0 +1,3 @@
+console.log('Hi! first console=))')
+console.log((true + false) > 2 + false)
+
--- test.js
+++
@@ -1,7 +0,0 @@
-console.log('Hi! Node.js')
-
-
-var a = true + true + true * 3;
-console.log(a)
-
-//The output is !!
