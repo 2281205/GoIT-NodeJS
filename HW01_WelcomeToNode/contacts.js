a3fc5e61ac8cbdb9e00bc2490fe0d17f9c8e934f
Mike Merzliakov <2281205@gmail.com>
2023-05-01 19:26:26 +0300

    add script

--- HW01_WelcomeToNode/contacts.js
+++ HW01_WelcomeToNode/contacts.js
@@ -1,21 +1,41 @@
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-    // ...твой код
-  }
-  
-  function getContactById(contactId) {
-    // ...твой код
-  }
-  
-  function removeContact(contactId) {
-    // ...твой код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твой код
-  }
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+const updateContacts = async (contacts) => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) { return null; }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
