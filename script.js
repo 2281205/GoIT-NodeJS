c286e412c87105360ea32f470ce28c0870814153
Mike Merzliakov <2281205@gmail.com>
2023-05-13 01:36:48 +0300

    add arg in script

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hi! test GIT')
+const argArr = JSON.parse(JSON.stringify(process.argv)).slice(2)
+
+if(!argArr.length) {console.log(`no argument`); process.exit(1)}
+
+argArr.forEach((item, index)=>{
+    console.log(`****`)
+    console.log(`arg index:${index+1}`)
+    console.log(`arg value:${item}`)
+    console.log(`****`)
+})
